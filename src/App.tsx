d6fe32b697eb5790fe111d65ebe76c23d46fabb9
박병조 <k97115284@gmail.com>
2025-02-12 15:40:40 +0900

    feat: 관리자 회원가입 페이지 생성 및 라우터 기본 세팅수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { BrowserRouter as RouterProvider } from 'react-router-dom'
+import Router from './utils/Router';
+
+
 function App() {
   return (
-    <>
-            <p className='font-pre-B m-1'>역할 : PM / Front-End Dev</p>
-            <p className='font-pre-M'>Web-RTC를 활용한 팀단위 화상회의</p>
-            <p className='font-pre-M'>실시간 문서 동시편집 및 일정공유 서비스</p>
-    </>
+    <RouterProvider>
+      <Router/>
+    </RouterProvider>
   )
 }
 
--- src/utils/Router.tsx
+++ src/utils/Router.tsx
@@ -1,7 +1,11 @@
 // import { Routes, Route, useNavigate } from 'react-router-dom';
 import { Routes, Route } from 'react-router-dom';
 
+// 화면정리 필수...! 추후 정리 할 예정이지만 다같이 화이팅 부탁드립니다 -박병조...25/02/12
 import Main from '../pages/Main';
+
+//
+import ManagerSignUp from '../pages/manager/SignUp';
 
 export default function Router() {
 
@@ -10,6 +14,7 @@ export default function Router() {
     return (
         <Routes location={location} key={location.pathname}>
             <Route path="/" element={<Main />} />
+            <Route path="/MSignUp" element={<ManagerSignUp />} />
         </Routes>
     );
 }
