402b48646d8731c70f009e3b8d70cca77430fd2f
박병조 <k97115284@gmail.com>
2025-02-12 15:22:07 +0900

    settting : router 파일 추가

---
+++ src/pages/Main.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Main: React.FC = () => {
+
+
+
+  return (
+    <div className="w-full h-full py-2 px-4 flex flex-col gap-9 mb-2">
+        Start Point
+    </div>
+  )
+};
+
+export default Main;
